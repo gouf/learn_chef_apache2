0868e17c1f3d78db0815548806810a4e932b684a
gouf <innocent.zero@gmail.com>
2018-08-04 14:06:16 +0900

    Install Apache2 and shows original index.html

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,23 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+apt_update 'Update the apt cache daily' do
+  frequency 86_400
+  action :periodic
+end
+
+package 'apache2'
+
+service 'apache2' do
+  supports status: true
+  action %i[enable start]
+end
+
+file '/var/www/html/index.html' do
+  content '<html>
+  <body>
+    <h1>hello world</h1>
+  </body>
+</html>'
+end
